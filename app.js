81abd93e4ce6f43b079d5bf23d4dc602a1743638
kiumad <mirfatemi7@gmail.com>
2022-07-01 16:20:10 +0430

    hours fixed

--- app.js
+++ app.js
@@ -18,12 +18,11 @@ setInterval(() => {
   let date = new Date();
   let sec_rotate = date.getSeconds() * 6;
   let min_rotate = date.getMinutes() * 6;
-  let hour_rotate = date.getHours() * 6;
+  let hour_rotate = date.getHours() * 30;
   sec_rotate += 6;
   if (date.getSeconds() === 60) min_rotate += 6;
-  if (date.getMinutes() === 60) hour_rotate += 6;
+  if (date.getMinutes() === 60) hour_rotate += 30;
   c_second.style.cssText = `transform: rotate(${sec_rotate}deg)`;
   c_min.style.cssText = `transform: rotate(${min_rotate}deg)`;
   c_hours.style.cssText = `transform: rotate(${hour_rotate}deg)`;
-  document.querySelector(".test").innerText = date.getMinutes() * 6;
 }, 1000);
